ba26b834b9caaa8fc1eef549a91d9a2381eb93ce
François Rozet <francois.rozet@outlook.com>
2020-11-08 21:05:37 +0100

    Update script generator

--- python/misc/slurm.py
+++ python/misc/slurm.py
@@ -25,28 +25,30 @@ python train.py {}
 
 run = ''
 
-for embedding in ['glove.6B.100d', 'glove.6B.300d', 'fasttext.simple.300d']:
-	for net in ['RNN', 'LSTM']:
-		for hidden in [64, 256]:
-			for layers in [1, 2]:
-				for dropout in [0, 0.5]:
-					for bidirectional in [False, True]:
-						biflag = '-bidirectional' if bidirectional else ''
+for dataset in ['IMDB', 'SST']:
+	for vsize in [25000, 50000]:
+		for embedding in ['glove.6B.100d', 'glove.6B.300d', 'fasttext.simple.300d']:
+			for net in ['RNN', 'LSTM', 'GRU']:
+				for hidden in [64, 256]:
+					for layers in [1, 2]:
+						for dropout in [0, 0.5]:
+							for bidirectional in [False, True]:
+								biflag = '-bidirectional' if bidirectional else ''
 
-						name = f'{net}-{hidden}-{layers}-{dropout}-{int(bidirectional)}-{embedding}'
-						formatted = content.format(
-							name,
-							name,
-							workers,
-							time,
-							f'-o {output} -net {net} -hidden {hidden} -layers {layers} -dropout {dropout} {biflag} -embedding {embedding} -workers {workers}'
-						)
+								name = f'{net}-{hidden}-{layers}-{dropout}-{int(bidirectional)}-{embedding}-{dataset}.{vsize}'
+								formatted = content.format(
+									name,
+									name,
+									workers,
+									time,
+									f'-o {output} -net {net} -hidden {hidden} -layers {layers} -dropout {dropout} {biflag} -embedding {embedding} -dataset {dataset} -vsize {vsize} -workers {workers}'
+								)
 
-						filename = f'{name}.sbatch'
-						with open(filename, 'w') as f:
-							f.write(formatted)
+								filename = f'{name}.sbatch'
+								with open(filename, 'w') as f:
+									f.write(formatted)
 
-						run += f'sbatch {filename}\n'
+								run += f'sbatch {filename}\n'
 
 with open('run.sh', 'w') as f:
 	f.write(run)
